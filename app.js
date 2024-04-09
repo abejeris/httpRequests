c74236c85353e1b7f5a1d3a2c5272e4b9edfad95
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-09 14:46:27 +0300

    added more http request solutions

--- app.js
+++ app.js
@@ -58,3 +58,45 @@
 // 	.catch((e) => {
 // 		console.log(e, 'error');
 // 	});
+
+//---------------------------------------------------------------------------
+// Refactoring fetch chains....
+
+// const checkStatusAndParse = (response) => {
+// 	if (!response.ok) throw new Error(`Status code error iz: ${response.status}`);
+// 	return response.json();
+// };
+
+// const printPlanets = (data) => {
+// 	console.log('Loaded 10 planets');
+// 	for (let planet of data.results) {
+// 		console.log(planet.name);
+// 	}
+// 	return Promise.resolve(data.next);
+// };
+
+// const fetchNextPlanets = (url = 'https://swapi.dev/api/planets') => {
+// 	return fetch(url);
+// };
+
+// fetchNextPlanets()
+// 	.then(checkStatusAndParse)
+// 	.then(printPlanets)
+// 	.then(fetchNextPlanets)
+// 	.then(checkStatusAndParse)
+// 	.then(printPlanets)
+// 	.then(fetchNextPlanets)
+// 	.then(checkStatusAndParse)
+// 	.then(printPlanets)
+// 	.then(fetchNextPlanets)
+// 	.then(checkStatusAndParse)
+// 	.then(printPlanets)
+// 	.then(fetchNextPlanets)
+// 	.then(checkStatusAndParse)
+// 	.then(printPlanets)
+// 	.then(fetchNextPlanets)
+// 	.then(checkStatusAndParse)
+// 	.then(printPlanets)
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
